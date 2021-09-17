da995e2fe57d05525f183e5e538522cfd64eaeb8
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2021-09-17 15:07:54 -0500

    Commit

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask, redirect, url_for, render_template
-#from flask_login import LoginManager, current_user
-#from werkzeug.urls import url_parse
+from flask import Flask, redirect, url_for, render_template, request, session
+from flask_mysqldb import MySQL
+import bcrypt
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-#login_manager = LoginManager(app)
+app.secret_key = "Pruebadecontraseñasecretacualquiera"
 
 @app.route("/")
 def home():
@@ -26,46 +25,9 @@ def carrito():
 def menu():
 	return render_template("carta.html")
 
-'''
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=['POST','GET'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = get_user(form.email.data)
-        if user is not None and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            next_page = request.args.get('next')
-            if not next_page or url_parse(next_page).netloc != '':
-                next_page = url_for('home')
-            return redirect(next_page)
-    return render_template('login.html', form=form)
+	return render_template("login.html")
 
-@app.route("/signup/", methods=["GET", "POST"])
-def show_signup_form():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = SignupForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        password = form.password.data
-        # Creamos el usuario y lo guardamos
-        user = User(len(users) + 1, name, email, password)
-        users.append(user)
-        # Dejamos al usuario logueado
-        login_user(user, remember=True)
-        next_page = request.args.get('next', None)
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home')
-        return redirect(next_page)
-    return render_template("signup.html", form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-'''
 if __name__ == "__main__":
 	app.run(debug = True)
