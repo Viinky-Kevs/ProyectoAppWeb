3b8cab24070afee512b12574e40ea5b7fb666916
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2021-10-08 14:58:08 -0500

    Commit

---
+++ static/javascript/script_carrito.js
@@ -0,0 +1,12 @@
+function incrementValue(){
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+function decreaseValue(){
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+}
